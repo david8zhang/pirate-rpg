ff9a2b2ade33e323f87b578aa9922e293ccf6c1b
David Zhang <david8zhang@berkeley.edu>
2021-05-30 14:26:57 -0700

    added configs for ship colliders

--- src/objects/Ship.ts
+++ src/objects/Ship.ts
@@ -1,3 +1,4 @@
+import { Direction } from '~/characters/Player'
 import Game from '../scenes/Game'
 
 export interface ShipConfig {
@@ -11,6 +12,9 @@ export interface ShipConfig {
     down: string
     side: string
   }
+  colliderConfig: {
+    left: any[]
+  }
 }
 
 export class Ship {
@@ -18,11 +22,13 @@ export class Ship {
   public hullSprite: Phaser.GameObjects.Sprite
   // public sailsSprite: Phaser.GameObjects.Sprite
   public scene: Game
+  public currDirection = Direction.LEFT
+  public wallImages: Phaser.Physics.Arcade.Image[] = []
 
   constructor(scene: Game, shipConfig: ShipConfig, position: { x: number; y: number }) {
     this.scene = scene
     const { x, y } = position
-    const { hullImages, sailsImages } = shipConfig
+    const { hullImages, sailsImages, colliderConfig } = shipConfig
     this.hullSprite = this.scene.add.sprite(x, y, hullImages.side)
     this.group = this.scene.add.group()
     // this.sailsSprite = this.scene.physics.add.sprite(x, y, sailsImages.side)
@@ -33,7 +39,7 @@ export class Ship {
     this.addCollider({ x, y }, { width: 264, height: 231 }, { x: -37, y: 80 })
     this.addCollider({ x, y }, { width: 100, height: 251 }, { x: -140, y: 50 })
     this.addCollider({ x, y }, { width: 100, height: 251 }, { x: 205, y: 50 })
-    this.setupWalls()
+    this.setupWalls(colliderConfig)
 
     this.group.add(this.hullSprite)
 
@@ -49,57 +55,17 @@ export class Ship {
     )
   }
 
-  setupWalls() {
-    // Walls for the middle part of the ship
-    this.addWall(this.hullSprite.x + 80, this.hullSprite.y + 70, 190, 2)
-    this.addWall(this.hullSprite.x + 80, this.hullSprite.y + 235, 190, 2)
-    this.addWall(this.hullSprite.x - 55, this.hullSprite.y + 125, 2, 100)
-    this.addWall(this.hullSprite.x + 210, this.hullSprite.y + 125, 2, 100)
-
-    // Walls for the stairs
-    this.addWall(this.hullSprite.x - 25, this.hullSprite.y + 55, 20, 2)
-    this.addWall(this.hullSprite.x - 45, this.hullSprite.y + 40, 20, 2)
-    this.addWall(this.hullSprite.x - 45, this.hullSprite.y + 100, 20, 2)
-
-    this.addWall(this.hullSprite.x + 182, this.hullSprite.y + 55, 20, 2)
-    this.addWall(this.hullSprite.x + 205, this.hullSprite.y + 40, 20, 2)
-    this.addWall(this.hullSprite.x + 200, this.hullSprite.y + 100, 20, 2)
-
-    // this.addWall(this.hullSprite.x - 30, this.hullSprite.y + 190, 10, 2)
-    this.addWall(this.hullSprite.x - 40, this.hullSprite.y + 180, 20, 2)
-    this.addWall(this.hullSprite.x - 15, this.hullSprite.y + 235, 20, 2)
-    this.addWall(this.hullSprite.x - 45, this.hullSprite.y + 220, 20, 2)
-
-    this.addWall(this.hullSprite.x + 195, this.hullSprite.y + 180, 20, 2)
-    this.addWall(this.hullSprite.x + 175, this.hullSprite.y + 235, 20, 2)
-    this.addWall(this.hullSprite.x + 200, this.hullSprite.y + 220, 20, 2)
-
-    // Walls for the upper decks
-    this.addWall(this.hullSprite.x - 90, this.hullSprite.y + 205, 80, 2)
-    this.addWall(this.hullSprite.x - 150, this.hullSprite.y + 190, 20, 2)
-    this.addWall(this.hullSprite.x - 165, this.hullSprite.y + 180, 20, 2)
-    this.addWall(this.hullSprite.x - 175, this.hullSprite.y + 170, 20, 2)
-    this.addWall(this.hullSprite.x - 185, this.hullSprite.y + 160, 20, 2)
-    this.addWall(this.hullSprite.x - 195, this.hullSprite.y + 150, 20, 2)
-    this.addWall(this.hullSprite.x - 200, this.hullSprite.y + 130, 2, 20)
-
-    this.addWall(this.hullSprite.x - 90, this.hullSprite.y + 40, 80, 2)
-    this.addWall(this.hullSprite.x - 150, this.hullSprite.y + 190, 20, 2)
-    this.addWall(this.hullSprite.x - 165, this.hullSprite.y + 180, 20, 2)
-    this.addWall(this.hullSprite.x - 175, this.hullSprite.y + 170, 20, 2)
-    this.addWall(this.hullSprite.x - 185, this.hullSprite.y + 160, 20, 2)
-    this.addWall(this.hullSprite.x - 195, this.hullSprite.y + 150, 20, 2)
-
-    this.addWall(this.hullSprite.x - 150, this.hullSprite.y + 50, 20, 2)
-    this.addWall(this.hullSprite.x - 165, this.hullSprite.y + 60, 20, 2)
-    this.addWall(this.hullSprite.x - 175, this.hullSprite.y + 70, 20, 2)
-    this.addWall(this.hullSprite.x - 185, this.hullSprite.y + 80, 20, 2)
-    this.addWall(this.hullSprite.x - 195, this.hullSprite.y + 90, 20, 2)
-    this.addWall(this.hullSprite.x - 205, this.hullSprite.y + 105, 20, 2)
-
-    this.addWall(this.hullSprite.x + 250, this.hullSprite.y + 40, 65, 2)
-    this.addWall(this.hullSprite.x + 250, this.hullSprite.y + 205, 65, 2)
-    this.addWall(this.hullSprite.x + 270, this.hullSprite.y + 120, 2, 150)
+  setupWalls(colliderConfig: any) {
+    const configs = colliderConfig[this.currDirection]
+    configs.forEach((wall) => {
+      const wallImg = this.addWall(
+        this.hullSprite.x + wall.xOffset,
+        this.hullSprite.y + wall.yOffset,
+        wall.width,
+        wall.height
+      )
+      this.wallImages.push(wallImg)
+    })
   }
 
   addCollider(
